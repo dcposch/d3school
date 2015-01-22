e2f040ea26bfc0e2ac6e94240cb942e36f76cad0
DC <dcposch@dcpos.ch>
2015-01-22 05:15:47 -0800

    Basic pwnage meter

--- demo.js
+++ demo.js
@@ -6,25 +6,45 @@ d3.json("games.json", function(error, json){
 });
 
 function draw(games){
-  var svg = d3.select("svg");
-  console.log(svg);
 
+  // Conventional margins
+  var margin = {top: 20, right:20, bottom: 40, left:40};
+  var width = 900 - margin.left - margin.right,
+      height = 600 - margin.left - margin.right;
+  var chart = d3.select("svg")
+    .append("g")
+      .attr("transform", "translate("+margin.left+","+margin.top+")");
+
+  // Scale
   var x = d3.scale.linear()
     .domain([1892,2015])
-    .range([10,800]);
+    .range([0,width]);
   var y = d3.scale.linear()
-    .domain([0,70])
-    .range([0,300]);
+    .domain([-50,50])
+    .range([height, 0]);
 
-  ["stanford", "california"].forEach(function(side){
-    var selection = svg.selectAll("rect."+side)
-        .data(games)
-      .enter().append("rect")
-        .attr("class", side)
-        .attr("x", function(d){return x(d.year);})
-        .attr("y", function(d){return side==="stanford" ? 400-y(d[side]) : 400;})
-        .attr("width", 6)
-        .attr("height", function(d){return y(d[side]);});
-  });
+  // Axes
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+  var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+  chart.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+  chart.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+  // Chart
+  var line = d3.svg.line()
+    .x(function(d) { return x(d.year); })
+    .y(function(d) { return y(d.stanford - d.california); });
+  chart.append("path")
+    .datum(games)
+    .attr("class", "line")
+    .attr("d", line);
 }
 
