ce338ec3cb86d7afd93e3f1342384926709b72a8
DC <dcposch@dcpos.ch>
2015-01-22 04:41:07 -0800

    Use d3.scale

--- demo.js
+++ demo.js
@@ -5,19 +5,26 @@ d3.json("games.json", function(error, json){
   draw(json);
 });
 
-function draw(games) {
+function draw(games){
   var svg = d3.select("svg");
   console.log(svg);
+
+  var x = d3.scale.linear()
+    .domain([1892,2015])
+    .range([10,800]);
+  var y = d3.scale.linear()
+    .domain([0,70])
+    .range([0,300]);
 
   ["stanford", "california"].forEach(function(side){
     var selection = svg.selectAll("rect."+side)
         .data(games)
       .enter().append("rect")
         .attr("class", side)
-        .attr("x", function(d){return (d.year - 1892)*10;})
-        .attr("y", function(d){return side==="stanford" ? 400-5*d[side] : 400;})
-        .attr("width", 8)
-        .attr("height", function(d){return 5*d[side];});
+        .attr("x", function(d){return x(d.year);})
+        .attr("y", function(d){return side==="stanford" ? 400-y(d[side]) : 400;})
+        .attr("width", 6)
+        .attr("height", function(d){return y(d[side]);});
   });
 }
 
